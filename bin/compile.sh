ee632c3f7cd38342e1713ae5e977fb0d31eac592
Max Amores <max1285@gmail.com>
2014-05-19 06:02:18 -0400

    updated scripts

--- bin/compile.sh
+++ bin/compile.sh
@@ -6,10 +6,12 @@ CLASSES=classes
 LIB=lib/*
 AWS=$HOME/aws-java-sdk-1.7.9/lib/*
 THIRD=$HOME/aws-java-sdk-1.7.9/third-party/*/*
+OPENCV=$HOME/opencv/build/bin/*
 
-CLASSPATH=$LIB:$AWS:$THIRD
+CLASSPATH=$LIB:$OPENCV:$AWS:$THIRD
 PROG=src/com/eamores/cloud/aws/recognition/RunRecognition.java
 SRC=src
+
 
 javac -d $CLASSES -sourcepath $SRC -classpath $CLASSPATH $PROG
 
--- bin/start_recognizer.sh
+++ bin/start_recognizer.sh
@@ -6,13 +6,15 @@ LIB=lib
 AWS=$HOME/aws-java-sdk-1.7.9/lib/*
 THIRDPARTY=$HOME/aws-java-sdk-1.7.9/third-party/*/*
 LIBRARY=$HOME/opencv/build/lib
+OPENCV=$HOME/opencv/build/bin/
 
 # Jar libraries
 lib_jar=`find $LIB -name *.jar | awk '{printf("%s:",$0)}'`
 aws_jar=`find $AWS -name *.jar | awk '{printf("%s:",$0)}'`
 lib_third=`find $THIRDPARTY -name *.jar | awk '{printf("%s:",$0)}'`
+opencv_jar=`find $OPENCV -name *.jar | awk '{printf("%s:",$0)}'`
 
-CLASSPATH=classes:$lib_jar:$aws_jar:$lib_third
+CLASSPATH=classes:$lib_jar:$aws_jar:$lib_third:$opencv_jar
 MAIN=com.eamores.cloud.aws.recognition.RunRecognition
 
 echo "Starting $MAIN" 
